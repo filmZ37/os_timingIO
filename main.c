99f3b4c0c452406aea5b314ffe0774e4669a5308
filmZ37 <phattharapolb@gmail.com>
2023-08-20 12:09:55 +0700

    adjust call_compute()

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ void call_io(){
     }
 };
 
-double call_compute(){
+void call_compute(){
     int sum=0;
     for(int i=0; i < SIZE; i++){
         sum += i;
@@ -30,7 +30,7 @@ int main(int argc,char** argv){
     printf("time used in I/O is %.6f\n",time);
 
     start=clock();
-    double result=call_compute();
+    result=call_compute();
     end=clock();
     time = ((double)end-start)/CLOCKS_PER_SEC;
     printf("time used in Computation is %.6f\n",time);
